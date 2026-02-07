3c2d76211b3845ad6d0dba8285d849caa402435f
hachiman-oct <176100494+hachiman-oct@users.noreply.github.com>
2026-02-07 22:37:07 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
     plugins: [react()],
     build: {
         rollupOptions: {
             input: {
-                main: './index.html',
-                cbt: './cbt-general/index.html',
-                mp3: './mp3-player/index.html',
-                text: './text-saver/index.html'
-            }
-        }
-    }
-})
+                // トップページがある場合
+                main: resolve(__dirname, 'index.html'),
+                // 各アプリの入り口を指定
+                cbt: resolve(__dirname, 'cbt-general/index.html'),
+                mp3: resolve(__dirname, 'mp3-player/index.html'),
+                text: resolve(__dirname, 'text-saver/index.html'),
+            },
+        },
+    },
+});
